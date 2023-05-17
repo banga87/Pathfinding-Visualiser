b4f613b8564c7670518f24ca8109be77b588e863
BingBong <angusroymcdonald@gmail.com>
2023-05-17 17:42:23 +1000

    select start + select finish

--- script.js
+++ script.js
@@ -1,17 +1,98 @@
+const cells = []
+let selectingStart = false;
+let selectingFinish = false;
+
+let selectStart = document.getElementById('select-start').addEventListener('click', () => {
+    selectingStart = true;
+});
+
+let selectFinish = document.getElementById('select-finish').addEventListener('click', () => {
+    selectingFinish = true;
+})
+
+let startTraversal = document.getElementById('start-traversal')
+
+
+// CREATE THE GRID
 const createGrid = () => {
     const grid = document.getElementById('grid')
 
     for (let i = 0; i < 46; i++) {
+        const row = []
         for (let j = 0; j < 71; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.dataset.position = `${i}-${j}`
-            grid.appendChild(cell);
+            const cellElement = document.createElement('div');
+            cellElement.classList.add('cell');
+            grid.appendChild(cellElement);
+
+            // CREATE THE CELL OBJECT
+            const cell = {
+                x: i,
+                y: j,
+                isStart: false,
+                isFinish: false,
+                isWall: false,
+                traversed: false,
+                element: cellElement
+            };
+
+            // ONCLICK SET THE CELL TO THE START LOCATION
+            cellElement.addEventListener('click', () => {
+                if (selectingStart) {
+                    removeStartLocation();
+                    cell.isStart = true;
+                    selectStart = false;
+                    cellElement.style.backgroundColor = 'green';
+                    selectingStart = false;
+                }
+            })
+
+            // ONCLICK SET THE CELL TO THE FINISH LOCATION
+            cellElement.addEventListener('click', () => {
+                if (selectingFinish) {
+                    removeFinishLocation();
+                    cell.isFinish = true;
+                    selectFinish = false;
+                    cellElement.style.backgroundColor = 'red';
+                    selectingFinish = false;
+                }
+            })
+            row.push(cell);
+        }
+        cells.push(row)
+    }
+}
+
+
+// REMOVE START LOCATION
+const removeStartLocation = () => {
+    for (let i = 0; i < cells.length; i++) {
+        for (let j = 0; j < cells[i].length; j++) {
+            if (cells[i][j].isStart) {
+                cells[i][j].isStart = false;
+                cells[i][j].element.style.backgroundColor = "antiquewhite";
+            }
         }
     }
 }
 
+
+// REMOVE FINISH LOCATION
+const removeFinishLocation = () => {
+    for (let i = 0; i < cells.length; i++) {
+        for (let j = 0; j < cells[i].length; j++) {
+            if (cells[i][j].isFinish) {
+                cells[i][j].isFinish = false;
+                cells[i][j].element.style.backgroundColor = "antiquewhite";
+            }
+        }
+    }
+}
+
+
 createGrid();
+
+
+
 
 // const breadthFirstSearch = () => {
 //     let visited = new Set();
